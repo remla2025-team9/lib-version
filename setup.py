96b3248ffe6af1670c7023c1fab2fe046a0c1837
iPersian <artin_sanaye@hotmail.com>
2025-05-05 03:47:53 +0200

    Add versioning automation and setup files

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import os
+
+# Dynamically load the version
+version = {}
+with open(os.path.join("lib_version", "__version__.py")) as f:
+    exec(f.read(), version)
+
+setup(
+    name="lib-version",
+    version=version["__version__"],
+    description="A reusable Python library for retrieving its own version.",
+    author="Your Team",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
