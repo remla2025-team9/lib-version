fdbadb2d8131e7d64e9726d10ff087214acd13b2
iPersian <artin_sanaye@hotmail.com>
2025-05-10 17:54:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     name="lib_version",
     version=version["__version__"],
     description="A reusable Python library for retrieving its own version.",
-    author="Your Team",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
