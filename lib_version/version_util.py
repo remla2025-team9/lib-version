74bc85e2d8f17f43ae6ba488c65af4deb010f6b1
iPersian <artin_sanaye@hotmail.com>
2025-06-09 15:16:30 +0200

    Use importlib.metadata to retrieve package version

--- lib_version/version_util.py
+++ lib_version/version_util.py
@@ -1,6 +1,14 @@
-from lib_version.__version__ import __version__
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+from . import version as local_version
 
 class VersionUtil:
     @staticmethod
     def get_version():
-        return __version__
+        try:
+            return version("lib-version")
+        except PackageNotFoundError:
+            return local_version.__version__
